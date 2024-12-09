2b91af6b47d9444873092eb45404d8c1d72d7280
Sena Ektiricioğlu <senaektiricioglu@gmail.com>
2024-12-09 23:44:38 +0300

    bitSet added

--- src/State.java
+++ src/State.java
@@ -1,14 +1,17 @@
+import java.util.BitSet;
 
 public class State {
     int row, col;
-    int moveCount = 0;
-    State parent;
+        int moveCount = 0;
+        State parent;
+        BitSet chessBoard; // Represents visited positions
 
-    State(int row, int col, State parent) {
-        this.row = row;
-        this.col = col;
-        this.parent = parent;
-        this.moveCount = (parent == null) ? 1 : parent.moveCount + 1; // Update move count
-    }
+        State(int row, int col, State parent, BitSet chessBoard) {
+            this.row = row;
+            this.col = col;
+            this.parent = parent;
+            this.moveCount = (parent == null) ? 1 : parent.moveCount + 1;
+            this.chessBoard = chessBoard;
+        }
 }
 
